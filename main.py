2093d352e230db11c08300098d84f3c3e53741fd
Nicolas Smith <nicolassmith22@gmail.com>
2025-12-02 09:32:04 -0800

    Refined the data file creation proccess. Added the time totals for positive and negative x values and their percentages to the data file.

--- main.py
+++ main.py
@@ -16,22 +16,24 @@ def lorenz(x,y,z, sigma, p, B):
     dzdt = x*y-B*z
     return dxdt, dydt, dzdt
 
+#Keep track of when x is positive and negative with a simple flag
 pos_time_count = []
 neg_time_count = []
 
+#Arrays to store values to be plotted and traced in the data file
 x_array = []
 y_array = []
 z_array = []
 t_array = []
 
+#Inital conditions
 x,y,z = 3,5,6
 dt = .01
 sigma, p, B = 10, 28, 8/3
-
 t = 0.0
 steps = 10000
 
-for n in range(steps):
+for n in range(steps+1):
     t_array.append(t)
     x_array.append(x)
     z_array.append(z)
@@ -60,13 +62,27 @@ for n in range(steps):
     
     t += dt
 
+#Time totals for positive and negative X values using the time count flags
+pos_time_total = sum(pos_time_count)
+neg_time_total = sum(neg_time_count)
+total_time = pos_time_total + neg_time_total
+pos_percent_time = (pos_time_total/total_time)*100
+neg_percent_time = (neg_time_total/total_time)*100
+
+#Create a unique file name
 run_id = uuid.uuid4().hex[:8]
 filename = f"data_run_{run_id}.csv"
+
 with open(filename, 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(["t", "x", "pos_flag", "neg_flag"])
+    
     for ti, xi, posf, negf in zip(t_array, x_array, pos_time_count, neg_time_count):
         writer.writerow([ti, xi, posf, negf])
+    
+    writer.writerow(["Total positive time","Percent Positive", "Total negative time", "Percent Negative"])
+    writer.writerow([pos_time_total,pos_percent_time, neg_time_total, neg_percent_time])
+
 
 plt.plot(x_array, z_array)
 plt.xlabel("X")
