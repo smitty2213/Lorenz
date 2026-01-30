906ec3cef9759dcd83df58e83fce42ac0b64244a
Nicolas Smith <nicolassmith22@gmail.com>
2026-01-29 21:19:13 -0800

    CSVReader can now read and caputre the mac dwell time

--- CSVReader.py
+++ CSVReader.py
@@ -1,12 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
-file = "data_run_6143af4a.csv"
-file2 = "run_level_data_8133c33b.csv"
+#file = "data_run_6143af4a.csv" #Big picture overview data
+file2 = "run_level_data_8133c33b.csv" #Run level data
 
-#Dataframes for each graph
-dataframe = pd.read_csv(file)
-run_level_dataframe = pd.read_csv(file2)
+#Dataframes to hold the data
+#dataframe = pd.read_csv(file)
+dataframe2 = pd.read_csv(file2)
+
+#Variables
+deltaT = 2 #Bin Width 
+cols = ["Pos Dwell Time", "Neg Dwell"]
+dataframe2[cols] = dataframe2[cols].apply(pd.to_numeric, errors = "coerce") #Turns any empty spaces from String to numbers and replace the dataframe with numberics
+maxT = dataframe2[cols].to_numpy() #Crate a numpy array
+maxT = np.nanmax(maxT) #Max value ignoring the blank space that would have this read as NaN
+
+
 
 #Mean Dwell Time Graph, commenting this out until needed
 '''y = dataframe.groupby("Changing Variable")
@@ -26,6 +36,7 @@ plt.legend(["Negative Dwell Time", "Positive Dwell Time"])
 plt.show()'''
 
 #Histogram for dwell time probabilities
+
 
 
 
